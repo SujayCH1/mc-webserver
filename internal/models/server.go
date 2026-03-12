dd6c6d3789a298fac6895cad883011de459ee802
Sujay Chanageri <suju.narayan@gmail.com>
2026-03-12 09:08:18 +0000

    migrations rollback

--- internal/models/server.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Server struct {
-	ID      int    `json:"id"`
-	Name    string `json:"name"`
-	Version string `json:"version"`
-	Port    string `json:"port"`
-	Status  string `json:"status"`
-}
--- migrations/0002_add_servers_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE servers (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    version TEXT,
-    port INTEGER UNIQUE,
-    status TEXT DEFAULT 'stopped'
-);
